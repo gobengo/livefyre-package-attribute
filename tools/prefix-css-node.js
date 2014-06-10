564926e9586c09a201ab9113de853069d23d9586
Benjamin Goering <bengoering@gmail.com>
2014-06-09 19:26:17 -0700

    adjust prefix-css-node to do better when this is a dep of the package you're building

--- tools/prefix-css-node.js
+++ tools/prefix-css-node.js
@@ -1,5 +1,8 @@
 var rework = require('rework');
-var packageJson = require('../package.json');
+var path = require('path');
 var prefix = require('./prefix-css');
+
+// Assume you're running this from the package root
+var packageJson = require(path.join(process.cwd(), 'package.json'));
 
 module.exports = prefix.bind({}, packageJson, rework);
