d11c954a2e4294e48792d0e5eba319e83603c47c
Benjamin Goering <bengoering@gmail.com>
2014-08-15 15:07:25 -0700

    0.1.1 use rework in node mode because it works better (but not in ie9 to use regex for browser

--- tools/prefix-css-node.js
+++ tools/prefix-css-node.js
@@ -1,7 +1,15 @@
 var path = require('path');
 var prefix = require('./prefix-css');
+var rework = require('rework');
 
 // Assume you're running this from the package root
 var packageJson = require(path.join(process.cwd(), 'package.json'));
 
-module.exports = prefix.bind({}, packageJson);
+module.exports = prefix.bind({}, packageJson, prefixCss);
+
+function prefixCss(prefix, css) {
+    var prefixedCss = rework(css)
+            .use(rework.prefixSelectors(prefix))
+            .toString();
+    return prefixedCss;
+}
--- tools/prefix-css-requirejs.js
+++ tools/prefix-css-requirejs.js
@@ -1,4 +1,31 @@
 var packageJson = require('json!package.json');
 var prefix = require('./prefix-css');
 
-module.exports = prefix.bind({}, packageJson);
+module.exports = prefix.bind({}, packageJson, prefixCss);
+
+function prefixCss(prefix, cssText) {
+    var match, results = [],
+        cssPattern = new RegExp("([^\\s][\\s\\S]*?)(\\{[\\s\\S]*?\\})", "g"),
+        selectors, prefixedSelectors;
+
+    while (match = cssPattern.exec(cssText)) {
+        //There might be a concatenation of selectors, explode them
+        selectors = match[1].split(",");
+
+        prefixedSelectors = [];
+
+        // if this is something like @font-face, it can't be prefixed so continue
+        if (selectors.length === 1 && selectors[0].trim().indexOf('@') === 0) {
+            results.push(selectors.join(','), match[2]);
+            console.log('skipping', match);
+            continue;
+        }
+
+        for (var i = 0, l = selectors.length; i < l; i += 1) {
+           prefixedSelectors.push(prefix + selectors[i]);
+        }
+        results.push(prefixedSelectors.join(","), match[2]);
+    }
+
+    return results.join("");
+};
